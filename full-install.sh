dbf6b821b14ae77a57aeac073fbe0da9a53f023b
Evilbas <dimonloboto@gmail.com>
2025-04-16 13:26:33 +0800

    +shikane

---
+++ full-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Base and random stuff
+yay -S kitty tmux fish neovim neofetch tree
+
+# Hyprland
+yay -S hyprland hyprlock hypridle sddm
+sudo systemctl enable sddm
+
+# Utils
+yay -S ags-hyprpanel-git network-manager-applet
+
+yay -S shikane
